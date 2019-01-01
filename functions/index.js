8dfa7aceb04ad73758d892ffd4e9bc235261a418
Jaani Leskinen <jaani.leskinen@fraktio.fi>
2019-01-02 00:40:07 +0200

    Firebase functions set up

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/hello', (req, res) => {
+  res.send('world!');
+});
+
+exports.webApi = functions.https.onRequest(main);
